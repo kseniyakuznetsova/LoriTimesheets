ef3beec6e18bb700e053ec4635000458ea55f75f
Kseniya Kuznetsova <kseniya9696@gmail.com>
2017-12-19 00:32:13 +0400

    Search Activity not all work

--- app/src/main/java/com/kuznetsova/loritimesheets/API/APIService.java
+++ app/src/main/java/com/kuznetsova/loritimesheets/API/APIService.java
@@ -1,12 +1,25 @@
 package com.kuznetsova.loritimesheets.API;
 
+import com.google.gson.JsonObject;
+import com.kuznetsova.loritimesheets.entities.ProjectParticipant;
+import com.kuznetsova.loritimesheets.entities.Task;
+import com.kuznetsova.loritimesheets.entities.TimeEntry;
+import com.kuznetsova.loritimesheets.entities.User;
 import com.kuznetsova.loritimesheets.entity.Token;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 
 public interface APIService {
@@ -14,4 +27,25 @@ public interface APIService {
     @Headers({"Content-Type: application/x-www-form-urlencoded","Authorization: Basic Y2xpZW50OnNlY3JldA=="})
     @POST("/app/rest/v2/oauth/token")
     Call<Token> signIn(@Field("grant_type") String grantType, @Field("username") String login, @Field("password") String password);
+
+    @GET("/app/rest/v2/userInfo")
+    Call<User> getUser(@Header("Authorization") String token);
+
+    @POST("/app/rest/v2/queries/ts$ProjectParticipant/getProjectsPart?view=projectParticipant-full")
+    Call<List<ProjectParticipant>> getProjectList(@Body JsonObject body, @Header("Authorization") String token);
+
+    @POST("/app/rest/v2/entities/ts$Task/search")
+    Call<List<Task>> getTaskList(@Body JsonObject body, @Header("Authorization") String token);
+
+    @POST("/app/rest/v2/entities/ts$TimeEntry")
+    Call<TimeEntry> createTimeEntry(@Body TimeEntry timeEntry, @Header("Authorization") String token);
+
+    @PUT("/app/rest/v2/entities/ts$TimeEntry/{id}")
+    Call<TimeEntry> updateTimeEntry(@Path("id") String id, @Body TimeEntry timeEntry, @Header("Authorization") String token);
+
+    @DELETE("/app/rest/v2/entities/ts$TimeEntry/{id}")
+    Call<Void> deleteTimeEntry(@Path("id") String id, @Header("Authorization") String token);
+
+    @POST("/app/rest/v2/queries/ts$TimeEntry/findTimeEntry?view=timeEntry-full")
+    Call<List<TimeEntry>> findTimeEntry(@Body JsonObject body, @Header("Authorization") String token);
 }
--- app/src/main/java/com/kuznetsova/loritimesheets/activity/WeekTimeEntryActivity.java
+++ app/src/main/java/com/kuznetsova/loritimesheets/activity/WeekTimeEntryActivity.java
@@ -2,23 +2,60 @@ package com.kuznetsova.loritimesheets.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.kuznetsova.loritimesheets.API.APIFactory;
+import com.kuznetsova.loritimesheets.API.APIService;
 import com.kuznetsova.loritimesheets.R;
+import com.kuznetsova.loritimesheets.entities.User;
+
+import java.io.Serializable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WeekTimeEntryActivity extends AppCompatActivity {
 
     private static final String TOKEN_PREFERENCE = "TOKEN_PREFERENCE";
     private static final String TOKEN_KEY = "TOKEN_KEY";
 
+    private APIService service;
+    private String token;
+    public User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_week_time_entry);
+
+        token = getSharedPreferences(TOKEN_PREFERENCE, Context.MODE_PRIVATE).getString(TOKEN_KEY, null);
+        service = APIFactory.getAPIService();
+        final Call<User> userCall = service.getUser("Bearer " + token);
+        userCall.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    user = response.body();
+                   /* weekPager = (ViewPager)findViewById(R.id.weekPager);
+                    WeekPagerAdapter adapter = new WeekPagerAdapter(getSupportFragmentManager());
+                    adapter.setUser(user);
+                    weekPager.setAdapter(adapter);
+                    weekPager.setCurrentItem(1);
+                    weekPager.setOffscreenPageLimit(3);*/
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+            }
+        });
     }
 
     @Override
@@ -33,5 +70,10 @@ public class WeekTimeEntryActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
+    }
+    public void onItemSearchClick(MenuItem item){
+        Intent intent = new Intent(this, SearchActivity.class);
+        intent.putExtra("user",(Serializable) user);
+        startActivity(intent);
     }
 }
